19c103d3068144168f5e49a7248b52e63309a698
ivanna 50 <jana.bagin@gmail.com>
2023-03-08 11:40:27 +0200

    hw2

--- app.js
+++ app.js
@@ -1,75 +1,22 @@
 
-// const event = require('node:events');
-//
-// const eventEmitter = new event();
-//
-// eventEmitter.on('click',()=>{
-//     console.log('Click, Click!!');
-// });
-//
-// eventEmitter.emit('click');
-// eventEmitter.emit('click');
-// eventEmitter.emit('click');
-// eventEmitter.emit('click');
-// eventEmitter.emit('click');
-// eventEmitter.emit('click');
-//
-// eventEmitter.once('ClickAndDie',()=>{
-//
-// console.log ('I am die');
-// });
-// eventEmitter.emit('ClickAndDie');
-// eventEmitter.emit('ClickAndDie');
-// eventEmitter.emit('ClickAndDie');
-//
-// const fs = require('node:fs');
-// const path = require('node:path');
-//
-// const readStream = fs.createReadStream(path.join('text','test.txt'),{encoding:'utf-8'});
-// const writeStream=fs.createWriteStream(path.join('text','test2.txt'))
-// readStream.on('data',(chunk)=>{
-//     writeStream.write(chunk);
-// })
-//
-// readStream.pipe(writeStream);
-
-
-
 const express=require('express');
 const app=express();
-const userDb=require('./userDb/user');
+const fsService =require('./fs.services');
 
-app.use(express.json());
-app.use(express.urlencoded({extended :true}));
+// const userDb=require('./userDb/users.json');
 
-// const users=[
-//     {
-//         name:'tania',
-//         age:25,
-//         gender:'female'
-//     },
-//     {
-//         name:'tonina',
-//         age:55,
-//         gender:'male'
-//     },
-//     {
-//         name:'clavaa',
-//         age:15,
-//         gender:'female'
-//     },
-//     {
-//         name:'anton',
-//         age:45,
-//         gender:'male'
-//     },
-// ]
-app.get('/user/1',(req, res)=>{
-    // res.json({user:'viktor'});
-    // res.send('users:anna');
-    res.json(userDb[1]);
+// app.use(express.json());
+// app.use(express.urlencoded({extended :true}));
 
-});
+
+app.get('/users',async (req, res)=>{
+    const users=await fsService.reader();
+    res.json(users);
+
+ })
+
+
+
 // app.get('/users/:userId',(req, res)=>{
 //
 //     const {userId}=req.params;
@@ -79,21 +26,28 @@ app.get('/user/1',(req, res)=>{
 //
 // })
 
-
-app.get ('/welcome',(req, res)=>{
-    res.send('WELCOME!!!WELCOME!!!')
-    // console.log('WELCOME!!!WELCOME!!!WELCOME!!!');
-    // res.end();
-});
-
-// app.post ('/users',(req, res)=>{
-//     const body=req.body;
-//     console.log(body);
 //
-// })
+// app.get ('/welcome',(req, res)=>{
+//     res.send('WELCOME!!!WELCOME!!!')
+//     console.log('WELCOME!!!WELCOME!!!WELCOME!!!');
+//     // res.end();
+// });
 
+// app.post ('/users',async (req, res)=>{
+//     const {name,age,gender}=req.body;
+//     const users=await fsService.reader();
+//
+//     const newUser={id:users[users.length-1]?.id+1 || 1, name, age, gender};
+//     users.push(newUser);
+//      await fsService.writer(users);
+//
+//
+//     res.status(201).json(newUser);
+//
+// });
+//
 
-const PORT= 5000;
+const PORT= 5100;
 
 app.listen(PORT,()=>{
     console.log(`server has started on PORT: ${PORT}`);
--- userDb/user.js
+++
@@ -1,7 +0,0 @@
-module.exports=[
-    {name:'Ivan',age:66},
-    {name:'Ivan',age:66},
-    {name:'Ivanna',age:63},
-    {name:'Ivan',age:66},
-    {name:'Ivani',age:16},
-]
